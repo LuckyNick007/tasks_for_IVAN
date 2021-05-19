f16a026e884cc4d172920be6e57391224fa44d06
LuckyNick007 <80332242+LuckyNick007@users.noreply.github.com>
2021-05-19 13:11:16 +0300

    Add files via upload

---
+++ 0.9 MENU.py
@@ -0,0 +1,11 @@
+# Допустим, вас попросили создать программу для ресторана: посетитель вводит,
+# какую еду он хотел бы заказать, и получает обратно предложение ресторана.
+
+# Ресторан только что открылся, поэтому в его меню всего несколько вариантов:
+
+# pizza: Margherita, Four Seasons, Neapolitan, Vegetarian, Spicy
+# salad: Caesar salad, Green salad, Tuna salad, Fruit salad
+# soup: Chicken soup, Ramen, Tomato soup, Mushroom cream soup
+
+# Если посетители просят что-то, чего нет в меню, программа должна написать «Sorry, we don't have it in the menu».
+
